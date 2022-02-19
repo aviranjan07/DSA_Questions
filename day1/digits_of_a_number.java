0eb2f0347fa61d97acb28aa2cc9ad57d9abe59e7
aviranjan07 <ad3447578@gmail.com>
2022-02-19 13:08:13 +0530

    7.digits of a number

---
+++ day1/digits_of_a_number.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class digits_of_a_number {
+    public static void main (String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int div = 1;
+        int temp = n;
+        while(temp >= 10){
+            temp = temp / 10;
+            div *= 10;
+        }
+        while (div >= 1){
+            int q = n / div;
+            int r = n % div;
+            System.out.println(q);
+            n = r;
+            div = div / 10;
+        }
+    }
+}
