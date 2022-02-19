f3343d1853bab84966a0ded47dd559a4456b53ea
aviranjan07 <ad3447578@gmail.com>
2022-02-19 19:26:02 +0530

    13.the curious case of bunjamin bulbs

---
+++ day1/the_curious_case_of_bunjamin_bulbs.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class the_curious_case_of_bunjamin_bulbs {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        
+        for(int i = 1; i * i <= n; i++){
+            System.out.println(i * i);
+        }
+    }
+}
