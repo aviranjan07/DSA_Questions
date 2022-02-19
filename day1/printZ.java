e3cd8897d54b2a7a96528efd8d58945b56f3c62a
aviranjan07 <ad3447578@gmail.com>
2022-02-19 07:59:08 +0530

    1.printZ

---
+++ day1/printZ.java
@@ -0,0 +1,19 @@
+// import java.util.*;
+
+// method 1 
+// public class printZ {
+//     public static void main (String[] args) {
+//         System.out.println("*****");
+//         System.out.println("   *");
+//         System.out.println("  *");
+//         System.out.println(" *");
+//         System.out.println("*****");
+//     }
+// }
+
+// method 2 
+public class printZ{
+    public static void main(String[] args) {
+        System.out.println("*****\n   *\n  *\n *\n*****");
+    }
+}
