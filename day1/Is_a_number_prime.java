d94005ff4338228ae0bd8775d45a725e718d7f67
aviranjan07 <ad3447578@gmail.com>
2022-02-19 09:32:27 +0530

    3.is a number prime

---
+++ day1/Is_a_number_prime.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Is_a_number_prime {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int t = scn.nextInt();
+
+        for(int i = 1; i <= t; i++) {
+            int x = scn.nextInt();
+ 
+            boolean isPrime = true;
+            int div = 2;
+            while (div * div <= x) {
+                int r = x % div;
+
+                if(r == 0){
+                   isPrime = false;
+                   break;
+                }
+                div++;
+            }
+            if (isPrime == true){
+                System.out.println("prime");
+
+            }else {
+                System.out.println("not prime");
+            }
+        }
+    }
+}
+/*
+simple input-
+5
+13
+2
+3
+4
+5
+*/
