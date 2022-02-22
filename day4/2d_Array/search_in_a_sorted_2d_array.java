05052f731efc08b4f87239effd6d71b4bbbeb727
aviranjan07 <ad3447578@gmail.com>
2022-02-22 17:58:26 +0530

    10.search in a sorted array

---
+++ day4/2d_Array/search_in_a_sorted_2d_array.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class search_in_a_sorted_2d_array {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int[][] arr = new int[n][n];
+        for(int i = 0; i < arr.length; i++) {
+            for ( int j = 0; j < arr[0].length; j++) {
+                arr[i][j] = scn.nextInt();
+            }
+        }
+        int x = scn.nextInt();
+        int i = 0;
+        int j = arr[0].length - 1;
+
+        while (i < arr.length && j >= 0) {
+            if(x == arr[i][j]) {
+                System.out.println(i);
+                System.out.println(j);
+                return;
+            }else if (x < arr[i][j]) {
+                j--;
+            }else {
+                i++;
+            }
+        }
+        System.out.println("Not Found");
+    }
+
+}
+
+/*
+input-
+4
+11
+12
+13
+14
+21
+22
+23
+24
+31
+32
+33
+34
+41
+42
+43
+44
+43
+output- 
+3
+2
+*/
