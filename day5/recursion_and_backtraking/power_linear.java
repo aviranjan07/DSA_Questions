0a350cfe24ca9444023457df6070ab7882f72706
aviranjan07 <ad3447578@gmail.com>
2022-02-23 11:43:57 +0530

    5.power linear

---
+++ day5/recursion_and_backtraking/power_linear.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class power_linear {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int x = scn.nextInt();
+        int n = scn.nextInt();
+        int xpn = power(x, n);
+        System.out.println(xpn);
+    }
+
+    public static int power(int x, int n){
+        if(n == 0) {
+            return 1;
+        }
+        int nm1p =  power(x, n -1 );
+        int xpn = nm1p * x;
+        return xpn;
+    }
+
+}
+/* 
+input-
+2
+5
+output-
+32
+*/
