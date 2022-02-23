a439db200c90f5c6f8961c530b33a16ab2fc6799
aviranjan07 <ad3447578@gmail.com>
2022-02-23 10:21:16 +0530

    3.print increasing and decreasing

---
+++ day5/recursion_and_backtraking/print_increasing_and_decreasing.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class print_increasing_and_decreasing {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        pdi(n);
+
+    }
+
+    public static void pdi(int n){
+        if (n == 0) {
+            return;
+        }
+        System.out.println(n);
+        pdi(n - 1);
+        System.out.println(n);
+        
+    }
+
+}
+/* 
+input- 
+3
+output-
+3
+2
+1
+1
+2
+3
+*/
