77b6446358f1818b7b26f824773d61bbdd07f33f
aviranjan07 <ad3447578@gmail.com>
2022-02-23 16:47:52 +0530

    12.first index

---
+++ day5/recursion_and_backtraking/fast_index.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class fast_index {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = scn.nextInt();
+        }
+        int d = scn.nextInt();
+        int fi = firstIndex(arr, 0, d);
+        System.out.println(fi);
+    }
+
+    public static int firstIndex(int[] arr, int idx, int x){
+        if(idx == arr.length) {
+            return - 1;
+        }
+        if (arr[idx] == x) {
+            return idx;
+        } else {
+            int fiisa = firstIndex(arr, idx + 1, x);
+            return fiisa;
+        }
+    }
+
+}
+/* 
+input-
+6
+15
+11
+40
+4
+4
+9
+4
+output-
+3
+*/
