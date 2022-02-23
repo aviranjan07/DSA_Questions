061d3931e72560434de88fa1cbf06e6ada67da76
aviranjan07 <ad3447578@gmail.com>
2022-02-23 12:21:45 +0530

    7.print zigzag

---
+++ day5/recursion_and_backtraking/print_zigzag.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class print_zigzag {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        pzz(n);
+    }
+
+    public static void pzz(int n){
+        if (n == 0) {
+            return;
+        }
+
+        System.out.print(n + " ");
+        pzz(n - 1);
+        System.out.print(n + " ");
+        pzz(n - 1);
+        System.out.print(n + " ");
+
+
+        
+    }
+
+}
+/* 
+input-
+3
+output-
+3 2 1 1 1 2 1 1 1 2 3 2 1 1 1 2 1 1 1 2 3
+*/
