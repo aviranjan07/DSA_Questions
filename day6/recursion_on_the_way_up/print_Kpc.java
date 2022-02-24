4ac44529c630b4a91d575ba5dbb0c6e48c9a9f48
aviranjan07 <ad3447578@gmail.com>
2022-02-24 10:10:43 +0530

    2.print kpc

---
+++ day6/recursion_on_the_way_up/print_Kpc.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+import introduction_to_recursion.print_increasing;
+
+public class print_Kpc {
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+        printKPC(str, "");
+   
+    }
+    static String[] codes = {".;", "abc" , "def" , "ghi", "jkl" , "mno", "pqrs", "tu", "vwx", "yz"};
+    public static void printKPC(String ques, String ans) {
+        if(ques.length() == 0){
+            System.out.println(ans);
+            return ;
+        }
+        char ch = ques.charAt(0);
+        String roq = ques.substring(1);
+
+        String codeforch = codes[ch - '0'];
+        for( int i = 0; i < codeforch.length(); i++) {
+            char cho = codeforch.charAt(i);
+            printKPC(roq, ans + cho);
+        }
+        
+    }
+
+}
