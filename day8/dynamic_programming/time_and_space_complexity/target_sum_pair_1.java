b9ba4f72c532c6867f5a0bcfa77aa03a6aecb952
aviranjan07 <ad3447578@gmail.com>
2022-02-26 11:24:44 +0530

    5.target sum pair 1

---
+++ day8/dynamic_programming/time_and_space_complexity/target_sum_pair_1.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class target_sum_pair_1 {
+
+  public static void targetSumPair(int[] arr, int target){
+    //write your code here
+    Arrays.sort(arr);
+
+    int li = 0;
+    int ri = arr.length - 1;
+    while( li < ri) {
+        if (arr[li] + arr[ri] < target) {
+            li++;
+        }else if (arr[li] + arr[ri] > target) {
+            ri--;
+        }else {
+            System.out.println(arr[li] + ", " + arr[ri]);
+            li++;
+            ri--;
+        }
+    }
+
+  }
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for(int i = 0 ;i < n; i++){
+      arr[i] = scn.nextInt();
+    }
+    int target = scn.nextInt();
+    targetSumPair(arr,target);
+  }
+
+}
+/* 
+input-
+12
+9 
+-48 
+100
+43
+84
+74
+86
+34
+-37
+60
+-29
+44
+160
+output-
+60, 100
+74, 86
+*/
